e7b060b596aa59942eedfa2636b565f0d603a2ae
Atay_qn28 <Atay_qn28@STHQ011A-11>
2021-11-01 17:48:01 +0400

    Add project files.

---
+++ Mini Task Manager/Commands/RelayCommand.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Mini_Task_Manager.Commands
+{
+	class RelayCommand : ICommand
+	{
+		event EventHandler ICommand.CanExecuteChanged
+		{
+			add
+			{
+				throw new NotImplementedException();
+			}
+
+			remove
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		bool ICommand.CanExecute(object parameter)
+		{
+			throw new NotImplementedException();
+		}
+
+		void ICommand.Execute(object parameter)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ Mini Task Manager/ViewModel/MainViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mini_Task_Manager.ViewModel
+{
+	public class MainViewModel:BaseViewModel
+	{
+	}
+}
